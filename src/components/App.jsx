f78a5de870a975b7446ea4e1dd20544ab5b8527d
vmhoang1703 <vmhoang1703@gmail.com>
2023-08-26 12:48:14 +0700

    Making Keeper App Using React.js

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+    return <div>
+        <Header></Header>
+        {notes.map((note) => (
+            <Note key = {note.key} title = {note.title} content = {note.content}></Note>
+        ))}
+        <Footer></Footer>
+    </div>
+}
+
+export default App;
