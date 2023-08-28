c5a46a71135aba2bd210a991d4189ea0689b8551
vmhoang1703 <vmhoang1703@gmail.com>
2023-08-28 19:45:38 +0700

    Set up Create Area Component and upadte CSS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
     return <div>
-        <Header></Header>
-        {notes.map((note) => (
-            <Note key = {note.key} title = {note.title} content = {note.content}></Note>
-        ))}
-        <Footer></Footer>
+      <Header />
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
+      <Footer />
     </div>
 }
 
